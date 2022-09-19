e5a2540fdc27ca20697ca8c9bc13e2e76fc60a90
Merotto Quentin <qmerotto@aol.com>
2022-09-19 18:21:07 +0200

    first handler

---
+++ web_api/handlers/tickets.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Ticket(ctx *gin.Context) {
+	body := ctx.Request.Body
+
+	_, err := io.ReadAll(body)
+	if err != nil {
+		log.Printf("read body error: %s", err.Error())
+		ctx.Status(http.StatusBadRequest)
+		return
+	}
+
+	ctx.Status(http.StatusAccepted)
+}
