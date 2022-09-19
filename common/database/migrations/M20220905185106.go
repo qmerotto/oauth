17d79e7191515bdd742a45b9214d80c6131e7ff5
Merotto Quentin <qmerotto@aol.com>
2022-09-19 18:20:57 +0200

    first migration

--- common/database/migrations/M20220905185106.go
+++ common/database/migrations/M20220905185106.go
@@ -9,20 +9,29 @@ var M20220905185106 = gormigrate.Migration{
 	ID: "20220905185106",
 	Migrate: func(tx *gorm.DB) error {
 		if err := tx.Exec(`
-			CREATE TABLE orders (
-				id BIGSERIAL PRIMARY KEY, value INT,
-				vat numeric NOT NULL DEFAULT 0.0,
-				total numeric NOT NULL DEFAULT 0.0
+			CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+			CREATE TABLE users (
+				uuid uuid PRIMARY KEY,
+				name varchar(32)
 			);
-			CREATE TABLE products (
-				id varchar(32) PRIMARY KEY,
-				name varchar(32) NOT NULL,
-				price numeric NOT NULL
+			CREATE TABLE clients (
+				id uuid PRIMARY KEY,
+				name varchar(32) NOT NULL
 			);
-			CREATE TABLE products_orders (
-				product_id varchar(32) NOT NULL,
-				order_id integer NOT NULL
+			CREATE TABLE ressource_servers (
+				id uuid PRIMARY KEY,
+				name varchar(32) NOT NULL
 			);
+
+			INSERT INTO users VALUES(uuid_generate_v4 (), 'user');
+			INSERT INTO users VALUES(uuid_generate_v4 (), 'admin');
+
+			INSERT INTO clients VALUES(uuid_generate_v4 (), 'web');
+			INSERT INTO clients VALUES(uuid_generate_v4 (), 'mobile');
+
+			INSERT INTO ressource_servers VALUES(uuid_generate_v4 (), 'ms_1');
+			INSERT INTO ressource_servers VALUES(uuid_generate_v4 (), 'ms_2');
+			INSERT INTO ressource_servers VALUES(uuid_generate_v4 (), 'front');
 		`).Error; err != nil {
 			return err
 		}
@@ -30,9 +39,9 @@ var M20220905185106 = gormigrate.Migration{
 	},
 	Rollback: func(tx *gorm.DB) error {
 		if err := tx.Exec(`
-			DROP TABLE orders;
-			DROP TABLE products;
-			DROP TABLE products_orders;
+			DROP TABLE users;
+			DROP TABLE clients;
+			DROP TABLE ressource_servers;
 		`).Error; err != nil {
 			return err
 		}
